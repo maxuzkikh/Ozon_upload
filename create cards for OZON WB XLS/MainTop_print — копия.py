fc57eeb658e6ad3af77455e455b62802996dd1e9
Max Uzkikh <35367909+maxuzkikh@users.noreply.github.com>
2024-08-17 01:35:58 +0500

    add
    add

--- create cards for OZON WB XLS/MainTop_print — копия.py
+++ create cards for OZON WB XLS/MainTop_print — копия.py
@@ -333,7 +333,7 @@ if os.path.exists(file_path):
             sys.exit(0)
             time.sleep(1)
         # Now calculate the offset
-        offset = -12580 - num_copies * 140
+        offset = -13580 - num_copies * 140
         process_image(print_path, rotate, layout_width)  # Pass layout_width as an argument
         offset += 40  # Increment offset for the next iteration
         offset_mouse=4.0*(num_copies+0)
