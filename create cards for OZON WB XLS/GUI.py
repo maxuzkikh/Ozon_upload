6bd724c7f570a9251f6048c09ce7a53c7183d216
Max Uzkikh <35367909+maxuzkikh@users.noreply.github.com>
2024-10-15 11:30:12 +0500

    te2
    te2

--- create cards for OZON WB XLS/GUI.py
+++ create cards for OZON WB XLS/GUI.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import filedialog
 import subprocess
+import os
+import win32com.client
 
-def run_script1():
+def run_script20():
     subprocess.Popen(["python", "generate_import_Ozon.py"])
 
 def run_script2():
@@ -45,6 +47,57 @@ def run_script13():
 def run_script15():
     subprocess.Popen(["python", "run_photoshop_create_cards.py"])
 
+def run_script16():
+    subprocess.Popen(["python", "fill_barcode_DB.py"])
+
+def run_script17():
+    # Get the current user's home directory
+    user_home = os.path.expanduser("~")
+
+    # Specify the path to the xlsx file using the user's home directory
+    xlsx_file_path = os.path.join(user_home, "Documents", "GitHub", "Ozon_upload", "DTF_images")
+
+    # Open the Excel file with the default program
+    try:
+        os.startfile(xlsx_file_path)
+        print(f"Opened: {xlsx_file_path}")
+    except FileNotFoundError:
+        print(f"The specified Excel file was not found: {xlsx_file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def run_script18():
+    # Get the current user's home directory
+    user_home = os.path.expanduser("~")
+
+    # Specify the path to the xlsx file using the user's home directory
+    xlsx_file_path = os.path.join(user_home, "Documents", "GitHub", "Ozon_upload","Tatulya","photoshop cards")
+
+    # Open the Excel file with the default program
+    try:
+        os.startfile(xlsx_file_path)
+        print(f"Opened: {xlsx_file_path}")
+    except FileNotFoundError:
+        print(f"The specified Excel file was not found: {xlsx_file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def run_script19():
+    # Get the current user's home directory
+    user_home = os.path.expanduser("~")
+
+    # Specify the path to the xlsx file using the user's home directory
+    xlsx_file_path = os.path.join(user_home, "Documents", "GitHub", "Ozon_upload","create cards for OZON WB XLS","data to create Термонаклейки","Fill_data.xlsx")
+
+    # Open the Excel file with the default program
+    try:
+        os.startfile(xlsx_file_path)
+        print(f"Opened: {xlsx_file_path}")
+    except FileNotFoundError:
+        print(f"The specified Excel file was not found: {xlsx_file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 root = tk.Tk()
 root.title("Python Script Runner")
 
@@ -58,8 +111,7 @@ button6.pack()
 button11 = tk.Button(root, text="photoshop_barcode_tati.py", command=run_script11)
 button11.pack()
 
-button1 = tk.Button(root, text="output_data_for_Ozon", command=run_script1)
-button1.pack()
+
 
 button2 = tk.Button(root, text="output_data_for_WB", command=run_script2)
 button2.pack()
@@ -94,8 +146,24 @@ button13.pack()
 button14 = tk.Button(root, text="update_stock", command=run_script14)
 button14.pack()
 
-button14 = tk.Button(root, text="1) run_photoshop_create_cards", command=run_script15)
-button14.pack()
+button17 = tk.Button(root, text="1) open folder Upload final DTF Images", command=run_script17)
+button17.pack()
+
+button16 = tk.Button(root, text="2) Fill Data Base Barcode", command=run_script16)
+button16.pack()
+
+button15 = tk.Button(root, text="3) run_photoshop_create_cards", command=run_script15)
+button15.pack()
+
+button18 = tk.Button(root, text="4) Export color Image for Ozon", command=run_script18)
+button18.pack()
+
+button19 = tk.Button(root, text="5) Fill Data to generate files for Ozon|WB", command=run_script19)
+button19.pack()
+
+button20 = tk.Button(root, text="6) output_data_for_Ozon", command=run_script20)
+button20.pack()
+
 
 
 
---
+++ create cards for OZON WB XLS/upload_DTF.py
@@ -0,0 +1,17 @@
+import os
+import win32com.client
+
+# Get the current user's home directory
+user_home = os.path.expanduser("~")
+
+# Specify the path to the xlsx file using the user's home directory
+xlsx_file_path = os.path.join(user_home, "Documents", "GitHub", "Ozon_upload", "DTF_images")
+
+# Open the Excel file with the default program
+try:
+    os.startfile(xlsx_file_path)
+    print(f"Opened: {xlsx_file_path}")
+except FileNotFoundError:
+    print(f"The specified Excel file was not found: {xlsx_file_path}")
+except Exception as e:
+    print(f"An error occurred: {e}")
